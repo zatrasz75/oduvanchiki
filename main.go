5c3b235a1a74920473c6c5ae71117ca1287d970c
zatrasz75 <zatrasz@ya.ru>
2022-12-23 10:20:06 +0300

    заготовка сделала

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"os"
+)
+
+var tpl = template.Must(template.ParseFiles("index.html"))
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tpl.Execute(w, nil)
+}
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	mux := http.NewServeMux()
+
+	fs := http.FileServer(http.Dir("assets"))
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+	mux.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":"+port, mux)
+}
