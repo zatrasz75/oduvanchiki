85058dcedfbf655bd0b133e6b9746e79a4500427
zatrasz75 <zatrasz@ya.ru>
2022-12-31 15:14:31 +0300

    Подключил gorilla/mux

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,13 +6,18 @@ import (
 	"net/http"
 )
 
+// Используем функцию template.ParseFiles() для чтения файлов шаблона.
+var (
+	tmpl = template.Must(template.ParseFiles("./ui/html/form.html"))
+)
+
 // Обработчик главной страницы.
 func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
-	// Инициализируем срез содержащий пути к двум файлам. Обратите внимание, что
+	// Инициализируем срез содержащий пути к трем файлам. Обратите внимание, что
 	// файл home.page.tmpl должен быть *первым* файлом в срезе.
 	files := []string{
 		"./ui/html/home.page.tmpl",
@@ -35,24 +40,42 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "внутренняя ошибка сервера", 500)
 	}
 }
+
+// Обработчик отображение страницы с формой
 func formPage(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/form" {
 		http.NotFound(w, r)
 		return
 	}
-	// Используем функцию template.ParseFiles() для чтения файлов шаблона.
-	ts, err := template.ParseFiles("./ui/html/form.html")
-	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "внутренняя ошибка сервера", 500)
-		return
+	data := ViewData{
+		Title: "Одуванчики",
+		Users: []string{"Krex", "Pex", "Fex"},
 	}
+
 	// Затем мы используем метод Execute() для записи содержимого
 	// шаблона в тело HTTP ответа. Последний параметр в Execute() предоставляет
 	// возможность отправки динамических данных в шаблон.
-	err = ts.Execute(w, nil)
+	err := tmpl.Execute(w, data)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "внутренняя ошибка сервера", 500)
 	}
 }
+
+// Обработчик сохранения данных страницы с формой
+func formSave(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/form" {
+		http.NotFound(w, r)
+		return
+	}
+	// Затем мы используем метод Execute() для записи содержимого
+	// шаблона в тело HTTP ответа. Последний параметр в Execute() предоставляет
+	// возможность отправки динамических данных в шаблон.
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "внутренняя ошибка сервера", 500)
+	}
+}
+
+// https://www.youtube.com/watch?v=lKvQYHZtuzA&list=PLHUicSITKZEmz2w3zo-aUpxCUZuqONE4c&index=2
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,25 +1,48 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"os"
+)
+
+type ViewData struct {
+	Title string
+	Users []string
+}
+
+// Инициализируем FileServer, он будет обрабатывать
+// HTTP-запросы к статическим файлам из папки "./ui/static".
+var (
+	fs = http.FileServer(http.Dir("./ui/static/"))
 )
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/form", formPage)
-	//mux.HandleFunc("/snippet/create", createSnippet)
 
-	// Инициализируем FileServer, он будет обрабатывать
-	// HTTP-запросы к статическим файлам из папки "./ui/static".
-	fs := http.FileServer(http.Dir("./ui/static/"))
+	PORT := ":4000"
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Использование номера порта по умолчанию: ", PORT)
+	} else {
+		PORT = ":" + arguments[1]
+	}
+	router := mux.NewRouter()
+	//	mux := http.NewServeMux()
+
+	router.HandleFunc("/", home).Methods("GET")
+	router.HandleFunc("/form", formPage).Methods("GET")
+	router.HandleFunc("/form", formSave).Methods("POST")
+
+	// Обработка всех url будет происходить через router
+	http.Handle("/", router)
 
 	// Используем функцию mux.Handle() для регистрации обработчика для
 	// всех запросов, которые начинаются с "/static/".
-	mux.Handle("/static/", http.StripPrefix("/static", fs))
+	router.Handle("/static/", http.StripPrefix("/static", fs))
 
-	log.Println("Запуск сервера на http://127.0.0.1:4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Print("Запуск сервера на http://127.0.0.1", PORT)
+	err := http.ListenAndServe(PORT, router)
 	log.Fatal(err)
 }
